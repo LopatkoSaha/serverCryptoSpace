7c0b6bd644f928eedb33e0574ff06e1abb965f8c
Andrei Lopatko <andrei.lopatko@together.com>
2024-04-21 16:31:13 +0300

    Update WS lib and fix style

---
+++ classes.ts
@@ -0,0 +1,102 @@
+class Car {
+  static type = "Car";
+
+  private gas = 0;
+
+  constructor(protected readonly consumption: number, private readonly gasCapacity: number, private readonly model: string) {}
+
+  go() {
+    const msg = `I was drive ${this.gas/this.consumption} miles in ${this.model}!`;
+    this.gas = 0;
+    return msg;
+  }
+
+  fuel(gas: number) {
+    const maxFuel = this.gasCapacity - this.gas;
+    if (maxFuel >= gas) {
+      this.gas = this.gas + gas;
+      return `Get fuel ${gas} litters`;
+    }
+    this.gas = this.gas + maxFuel;
+    return `Get fuel ${maxFuel} litters`;
+  }
+
+  whoYouAre() {
+    return `I am object type ${Car.type} and model ${this.model}`;
+  }
+
+  static calcShtraf() {
+    return new Date().getHours();
+  }
+}
+
+class BMW extends Car {
+  constructor(consumption: number, gasCapacity: number) {
+    super(consumption, gasCapacity, "BMW");
+  }
+
+  whoYouAre() {
+    return `I am cool BMW with consumption = ${this.consumption}`;
+  }
+}
+
+class BMW_X5 extends BMW {
+
+}
+
+class User {
+  constructor(private name: string, private hesCar?: BMW) {
+
+  }
+
+  goToSilpo () {
+    if (this.hesCar) {
+      this.hesCar.whoYouAre();
+      this.hesCar.BMW_lol();
+      if (this.hesCar instanceof BMW) {
+        this.hesCar.BMW_lol();
+      }
+      //......
+    }
+  }
+}
+
+class Opel extends Car {
+  constructor(consumption: number, gasCapacity: number) {
+    super(consumption, gasCapacity, "Opel");
+  }
+}
+
+// const bmw = new BMW_X5(10, 50);
+// console.log(bmw.go());
+// console.log(bmw.fuel(60));
+// console.log(bmw.go());
+console.log(Car.calcShtraf());
+
+const opelVectra = new Opel(10, 52);
+const Vasya = new User("Vasya", new Opel(10, 20))
+
+type User_1 = {
+  age: number,
+  weight: number,
+  gender: "male" | "female";
+}
+
+type Car_1 = {
+  age: number[],
+  weight: number,
+}
+
+type BMW_1 = Car_1 & {
+  age: number[],
+  weight: number,
+  lol: string,
+}
+
+function lol (item: Car_1) {
+  return item.age.map((item) => item.)
+};
+
+const user = { age: [10], weight: 1 as const, gender: "male" as const, lol: "kek"};
+
+lol(user)
